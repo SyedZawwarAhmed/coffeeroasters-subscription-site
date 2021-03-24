65fb9cfa1a394e5f719cd091871a8b5ec819852c
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-03-24 23:56:15 +0500

    changes made

--- starter-code/assets/plan/plan.js
+++ starter-code/assets/plan/plan.js
@@ -1,9 +1,9 @@
-const toggler = document.getElementsByClassName("toggler");
-const orderCards = document.getElementsByClassName("order-cards");
+const toggler = document.getElementsByClassName("toggler"),
+  orderCards = document.getElementsByClassName("order-cards");
 
-for (let i=0; i<toggler.length; i++) {
+for (let i = 0; i < toggler.length; i++) {
   toggler[i].addEventListener("click", () => {
     toggler[i].classList.toggle("toggler-rotated");
     orderCards[i].classList.toggle("collapsed");
-  })
+  });
 }
