b150ebfdc9fcdb6ffbbfae15598d6207aeeeffc5
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-06-13 22:39:56 +0500

    changes made

--- assets/hamburger.js
+++ assets/hamburger.js
@@ -1,8 +1,8 @@
-const menuContainer = document.getElementById("menu-container"), 
-menu = document.getElementById("menu"),
-hamburger = document.getElementById("hamburger"),
-links = document.getElementsByClassName("hamburger-links"),
-nav = document.getElementById("nav");
+const menuContainer = document.getElementById("menu-container"),
+  menu = document.getElementById("menu"),
+  hamburger = document.getElementById("hamburger"),
+  links = document.getElementsByClassName("hamburger-links"),
+  nav = document.getElementById("nav");
 
 let opened = false;
 
@@ -10,9 +10,11 @@ hamburger.addEventListener("click", () => {
   opened = opened === false ? true : false;
 
   if (opened) {
-    nav.style.backgroundColor = "transparent"
+    hamburger.innerHTML =
+      '<div id="hamburger" class="hamburger"><img src="assets/shared/mobile/icon-close.svg" alt=""></div>';
+    nav.style.backgroundColor = "transparent";
     menu.classList.toggle("menu-expanded");
-    menuContainer.style.zIndex = "2"
+    menuContainer.style.zIndex = "2";
 
     for (let i = 0; i < 3; i++) {
       links[i].style.pointerEvents = "all";
@@ -21,6 +23,8 @@ hamburger.addEventListener("click", () => {
       }, 250);
     }
   } else {
+    hamburger.innerHTML =
+      '<div id="hamburger" class="hamburger"><img src="assets/shared/mobile/icon-hamburger.svg" alt=""></div>';
     for (let i = 0; i < 3; i++) {
       links[i].classList.toggle("visible");
       links[i].style.pointerEvents = "none";
@@ -28,7 +32,7 @@ hamburger.addEventListener("click", () => {
     setTimeout(() => {
       menu.classList.toggle("menu-expanded");
     }, 250);
-    
+
     setTimeout(() => {
       menuContainer.style.zIndex = "-2";
       nav.style.backgroundColor = "#fff";
