c41d98f56bddce2702ad23e0145b4db37cc62b78
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-06-13 22:02:48 +0500

    changes made

--- starter-code/assets/hamburger.js
+++ starter-code/assets/hamburger.js
@@ -1,7 +1,8 @@
 const menuContainer = document.getElementById("menu-container"), 
 menu = document.getElementById("menu"),
-  hamburger = document.getElementById("hamburger"),
-  links = document.getElementsByClassName("hamburger-links");
+hamburger = document.getElementById("hamburger"),
+links = document.getElementsByClassName("hamburger-links"),
+nav = document.getElementById("nav");
 
 let opened = false;
 
@@ -9,7 +10,7 @@ hamburger.addEventListener("click", () => {
   opened = opened === false ? true : false;
 
   if (opened) {
-    
+    nav.style.backgroundColor = "transparent"
     menu.classList.toggle("menu-expanded");
     menuContainer.style.zIndex = "2"
 
@@ -27,9 +28,10 @@ hamburger.addEventListener("click", () => {
     setTimeout(() => {
       menu.classList.toggle("menu-expanded");
     }, 250);
-
+    
     setTimeout(() => {
       menuContainer.style.zIndex = "-2";
+      nav.style.backgroundColor = "#fff";
     }, 500);
   }
 });
